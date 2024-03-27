f9cd36e03b10022faf8d549b3dc26dd5b896fc72
katllv <katharinavu@hotmail.com>
2024-03-27 01:05:36 +0100

    har rykket lidt rundt og ændret navne og fikset ting og sådan wuhu

---
+++ Web-Server/Frontend/js/data.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+	updateData();
+	setInterval(updateData, 1000); //opdaterer hvert 1000 ms
+});
+
+//DATAAAAAAAA
+function updateData() {
+	fetch("./spi_data.txt")
+		.then((response) => response.text())
+		.then((data) => {
+			// Split the data by newline and get the last line
+			var lines = data.trim().split("\n");
+			var latestValue = lines[lines.length - 1];
+			document.getElementById("spiValue").innerText = latestValue;
+			console.log("SPI value: ", latestValue);
+		});
+}
