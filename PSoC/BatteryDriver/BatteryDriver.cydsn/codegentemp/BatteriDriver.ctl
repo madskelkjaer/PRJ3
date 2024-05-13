fbcafdccc95f67552341a011cd7fbb821d082762
AndreasStrander <150347313+AndreasStrander@users.noreply.github.com>
2024-05-13 12:14:02 +0200

    BatteryDriver
    Hej allesammen. Her er der en driver til batterikontrollen. Velbekomme og god dag :)

---
+++ PSoC/BatteryDriver/BatteryDriver.cydsn/codegentemp/BatteriDriver.ctl
@@ -0,0 +1,9 @@
+-- ======================================================================
+-- BatteriDriver.ctl generated from BatteriDriver
+-- 05/13/2024 at 09:57
+-- This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
+-- ======================================================================
+
+-- PSoC Clock Editor
+-- Directives Editor
+-- Analog Device Editor
---
+++ PSoC/BatteryDriver/BatteryDriver.cydsn/main.c
@@ -0,0 +1,37 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include"BatteryDriver.h"
+
+
+
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+    char UartBuffer[256];
+    UART_1_Start();
+    ADC_SAR_1_Start();
+    ADC_SAR_1_StartConvert();
+    ADC_SAR_2_Start();
+    ADC_SAR_2_StartConvert();
+
+    for(;;)
+    { 
+        //snprintf(uartBuffer, sizeof(uartBuffer), "Voltage divider: %i Percent\r\n", voltageDividerToPercent());
+        snprintf(UartBuffer, sizeof(UartBuffer), "Current hall: %i Volts\r\n", currentHall());
+        UART_1_PutString(UartBuffer); 
+    }
+}
+
+
+
+/* [] END OF FILE */
