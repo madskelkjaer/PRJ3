e719b43c704d1c2c505a1a247c8aba6949018ccc
Mads Kampmann Elkjær <madse1998@gmail.com>
2024-05-14 15:45:06 +0200

    Create test.py

---
+++ Web-Server/Backend/test.py
@@ -0,0 +1,27 @@
+import spidev
+import time
+
+### SPI DEV ###
+spi = spidev.SpiDev()
+bus = 0
+device = 0
+spi.open(bus, device)
+
+spi.max_speed_hz = 1000
+spi.mode = 0
+
+while True:
+    inputtet = input("Enter data to send: ")
+    
+    print("----")
+    print("Sending data: ", inputtet)
+    try:
+        response = spi.xfer(inputtet)
+        print("Data sent: ", inputtet)
+        print("Data received: ", response)
+        print("----")
+    except Exception as e:
+        print("Error in sending data: ", e)
+
+
+
