1dae71b7460e6b39b10152225a2ba37535e9f680
Mads Kampmann Elkjær <madse1998@gmail.com>
2024-05-14 15:46:16 +0200

    Update test.py

--- Web-Server/Backend/test.py
+++ Web-Server/Backend/test.py
@@ -16,7 +16,7 @@ while True:
     print("----")
     print("Sending data: ", inputtet)
     try:
-        response = spi.xfer(int(inputtet))
+        response = spi.xfer([int(inputtet)])
         print("Data sent: ", inputtet)
         print("Data received: ", response)
         print("----")
