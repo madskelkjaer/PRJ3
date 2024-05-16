b4d4da6f331a2bd792eb96986fae87a68d982e47
emilieweiss <emilieweiss@outlook.com>
2024-05-16 15:41:32 +0200

    Noget på styringssiden

--- Web-Server/Frontend/js/styring.js
+++ Web-Server/Frontend/js/styring.js
@@ -4,7 +4,7 @@ var i;
 for (i = 0; i < acc.length; i++) {
 	acc[i].addEventListener("click", function () {
 		/* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
+	to highlight the button that controls the panel */
 		this.classList.toggle("active");
 
 		/* Toggle between hiding and showing the active panel */
