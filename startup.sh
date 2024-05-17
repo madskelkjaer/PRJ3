276fe99d42ffd336f31cb5e6e4f3b21a75a99c38
Mads Kampmann Elkjær <madse1998@gmail.com>
2024-05-17 13:26:24 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-echo "STARTER SERVER"
+echo "STARTER SERVER" >> $LOGFILE 2>&1
 
-cd /home/perhi/PRJ3 || exit
+sleep 30
 
-git pull
+cd /home/perhi/PRJ3 || { echo "Failed to change directory to /home/perhi/PRJ3" >> $LOGFILE 2>&1; exit 1; }
 
-ngrok http --domain=capital-renewing-jennet.ngrok-free.app 5000 &
+/usr/bin/git pull >> $LOGFILE 2>&1
 
-cd /home/perhi/PRJ3/Web-Server/Backend/ || exit
-flask run --host=0.0.0.0 --port=5000 &
+/usr/local/bin/ngrok http --domain=capital-renewing-jennet.ngrok-free.app 5000 >> $LOGFILE 2>&1 &
 
-echo "Backend started"
+sleep 5
+
+cd /home/perhi/PRJ3/Web-Server/Backend/ || { echo "Failed to change directory to /home/perhi/PRJ3/Web-Server/Backend/" >> $LOGFILE 2>&1; exit 1; }
+/usr/bin/flask run --host=0.0.0.0 --port=5000 >> $LOGFILE 2>&1 &
+
+echo "Backend started" >> $LOGFILE 2>&1
