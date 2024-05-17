8abe1f159cd6f8548193f0e1c77fe1661926cb91
Mads Kampmann Elkjær <madse1998@gmail.com>
2024-05-17 12:53:52 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,11 +2,13 @@
 
 echo "STARTER SERVER"
 
-cd /home/perhi/PRJ3
+cd /home/perhi/PRJ3 || exit
+
 git pull
 
 ngrok http --domain=capital-renewing-jennet.ngrok-free.app 5000 &
 
-cd /Web-Server/Backend/
+cd /home/perhi/PRJ3/Web-Server/Backend/ || exit
 flask run --host=0.0.0.0 --port=5000 &
+
 echo "Backend started"
