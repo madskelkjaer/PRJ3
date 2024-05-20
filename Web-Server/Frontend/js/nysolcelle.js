f099bbb2755dc243d6539c7b0d4ab1f1bb90ce7f
katllv <katharinavu@hotmail.com>
2024-05-20 21:56:35 +0200

    test

--- Web-Server/Frontend/js/nysolcelle.js
+++ Web-Server/Frontend/js/nysolcelle.js
@@ -11,11 +11,7 @@ document.getElementById("maxDown").textContent = maxDown;
 document.getElementById("maxRight").textContent = maxRight;
 document.getElementById("maxLeft").textContent = maxLeft;
 
-// Initialize elevation and azimuth
-let elevation = 0;
-let azimuth = 0;
-
-// Set home/return position
+//Set home/returnposition ----------------------------------------------------
 let homeX = 0;
 let homeY = 0;
 
@@ -34,66 +30,21 @@ function updateHome() {
 	}
 }
 
-// Function to send data to server
+// Funktion til API kald for at sende data til server
 function moveSolarCell(direction) {
-	fetch(`https://capital-renewing-jennet.ngrok-free.app/api/move/${direction}`)
-		.then((response) => {
-			if (response.ok) {
-				return response.json();
-			} else {
-				throw new Error("API request failed");
-			}
-		})
-		.then((data) => {
-			console.log(data);
-		})
-		.catch((error) => {
-			console.error(error);
-		});
+	fetch(
+		`https://capital-renewing-jennet.ngrok-free.app/api/move/${direction}`
+	).then((response) => {
+		if (response.ok) {
+			return response.json();
+		} else {
+			throw new Error("API request failed");
+		}
+	});
 }
 
-// Event listener for the "Update" button
-document.getElementById("updateButton").addEventListener("click", updateHome);
-
-// Handle arrow key presses
-document.addEventListener("keydown", (event) => {
-	event.preventDefault();
-	switch (event.key) {
-		case "ArrowUp":
-			if (elevation >= maxUp) {
-				console.log("for mange grader til op!!!");
-			} else {
-				moveSolarCell("up");
-			}
-			break;
-		case "ArrowDown":
-			if (elevation <= maxDown) {
-				console.log("for mange grader til ned!!!!");
-			} else {
-				moveSolarCell("down");
-			}
-			break;
-		case "ArrowLeft":
-			if (azimuth <= maxLeft) {
-				console.log("for mange grader til venstre!!!!");
-			} else {
-				moveSolarCell("left");
-			}
-			break;
-		case "ArrowRight":
-			if (azimuth >= maxRight) {
-				console.log("for mange grader til højre!!!");
-			} else {
-				moveSolarCell("right");
-			}
-			break;
-	}
-});
-
 document.addEventListener("DOMContentLoaded", function () {
-	const ws = new WebSocket(
-		"wss://capital-renewing-jennet.ngrok-free.app/api/ws"
-	);
+	const ws = new WebSocket("//capital-renewing-jennet.ngrok-free.app/api/ws");
 	ws.onopen = () => {
 		console.log("Connected to WS fra solcelleData");
 	};
@@ -106,8 +57,8 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 const updateWSData = (message_data) => {
-	azimuth = message_data.azimuth;
-	elevation = message_data.elevation;
+	let azimuth = message_data.azimuth;
+	let elevation = message_data.elevation;
 	console.log("azimuth: ", azimuth);
 	console.log("elevation: ", elevation);
 	solarcell1.style.transform = `rotateX(${elevation}deg)`; //drejer solcellen
@@ -118,7 +69,52 @@ const updateWSData = (message_data) => {
 	console.log(`Azimuth lige nu: ${azimuth} deg`);
 };
 
-// Event listeners for buttons
+// Event listener for the "Update" button
+document.getElementById("updateButton").addEventListener("click", updateHome);
+
+//MED PILETASER -----------------------------------------------
+document.addEventListener("keydown", (event) => {
+	event.preventDefault();
+	//hvis man trykker en tast ned
+	switch (event.key) {
+		case "ArrowUp":
+			if (elevation >= maxUp) {
+				console.log("for manage grader op!!!");
+				break;
+			} else {
+				moveSolarCell("up");
+				break;
+			}
+		case "ArrowDown":
+			if (elevation <= maxDown) {
+				console.log("for mange grader ned!!!!");
+				break;
+			} else {
+				moveSolarCell("down");
+				break;
+			}
+		case "ArrowLeft":
+			if (azimuth <= maxLeft) {
+				console.log("for mange grader til venstre!!!!");
+				break;
+			} else {
+				moveSolarCell("left");
+				break;
+			}
+		case "ArrowRight":
+			if (azimuth >= maxRight) {
+				console.log("for mange grader til højre!!!");
+			} else {
+				moveSolarCell("right");
+				break;
+			}
+	}
+});
+
+//MED KNAPPERNE -----------------------------------------------
+
+let rotateInterval;
+
 document.getElementById("rotateUp").addEventListener("mousedown", () => {
 	if (elevation >= maxUp) {
 		console.log("for mange grader til op!!!");
@@ -152,7 +148,43 @@ document.getElementById("rotateLeft").addEventListener("mousedown", () => {
 });
 
 document.getElementById("reset").addEventListener("mousedown", () => {
-	azimuth = homeX;
+	azimuth = homeX; //omvendt end hvad man forventer lol
 	elevation = homeY;
 	moveSolarCell("home");
 });
+
+document.addEventListener("mouseup", () => {
+	clearInterval(rotateInterval);
+});
+
+//MED MUSEN -----------------------------------------------
+// const solarcell = document.querySelector(".solarcell");
+// let mouseX = 0;
+// let mouseY = 0;
+// let isMouseDown;
+// const rotationValue = 360;
+
+// solarcell.addEventListener("mousedown", (e) => {
+// 	isMouseDown = true;
+// 	prevX = e.clientX;
+// 	console.log("mousedown");
+// });
+
+// document.addEventListener("mouseup", () => {
+// 	isMouseDown = false;
+// 	console.log("mouseup");
+// });
+
+// document.addEventListener("mousemove", (e) => {
+// 	if (isMouseDown) {
+// 		mouseX = e.clientX;
+// 		mouseY = e.clientY;
+// 		rotateX = -(mouseY / window.innerHeight - 0.5) * rotationValue;
+// 		rotateY = (mouseX / window.innerWidth - 0.5) * rotationValue;
+// 		console.log("hej");
+// 		solarcell.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+// 		console.log("mousemove");
+// 	}
+// });
+
+//----------------------------------------------------------------------------
